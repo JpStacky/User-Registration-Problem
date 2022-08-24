a0f3334c8528feaf1243da97eac0f522196affdd
Jp <jyotiprakashpc@outlook.com>
2022-08-24 21:21:03 +0530

    Use MSTest Validate user

---
+++ MSTestUC10.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RegexProblem
+{
+    [TestClass]
+    public class MSTestUC10
+    {
+        string result;
+        ValidateUserUC10 validateUserUC10 = new ValidateUserUC10();
+        
+        [TestMethod]
+        public void check_First_Name()
+        {
+            result = validateUserUC10.ValidateFirstName("Hari");
+            Assert.AreEqual("Happy",result);
+        }
+        
+        [TestMethod]
+        public void check_Last_Name()
+        {
+            result = validateUserUC10.ValidateFirstName("Kumar");
+            Assert.AreEqual("Happy", result);
+        }
+
+        [TestMethod]
+        public void check_Email()
+        {
+            result = validateUserUC10.ValidateFirstName("erjprakash22@gmail.com");
+            Assert.AreEqual("Happy", result);
+        }
+
+        [TestMethod]
+        public void check_Phone_Number()
+        {
+            result = validateUserUC10.ValidateFirstName("7008725250");
+            Assert.AreEqual("Sad", result);
+        }
+
+        [TestMethod]
+        public void check_Password()
+        {
+            result = validateUserUC10.ValidateFirstName("Birdin@theSky");
+            Assert.AreEqual("Happy", result);
+        }
+    }
+}
---
+++ ValidateUserUC10.cs
@@ -0,0 +1,58 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegexProblem
+{
+    public class ValidateUserUC10
+    {
+        public string validFirstName = "^[A-Z]{1}[a-z]*$";
+        public string validLastName = "^[A-Z]{1}[a-z]*$";
+        public string validEmail = "^[a-zA-Z0-9]([.]{0,1}[a-zA-Z0-9])*[@]{1}[a-zA-Z0-9]{1,}[.]{1}[a-zA-Z]{2,3}([.]{1}[a-zA-Z]{2,3}){0,1}$";
+        public string validMobileNumber = "^91[ ][0-9]{10}$";
+        public string validPassword = "^(?=.*?[A - Z])(?=.*?[a - z])(?=.*?[0 - 9])(?=.*?[#?!@$%^&*-]).{8,}$";
+        public string choice;
+        public string ValidatePassword(string password)
+        {
+            if (Regex.IsMatch(password, validPassword))
+                choice = "Happy";
+            else
+                choice = "Sad";
+            return choice;
+        }
+        public string ValidateMobileNumber(string mobileNumber)
+        {
+            if (Regex.IsMatch(mobileNumber, validMobileNumber))
+                choice = "Happy";
+            else
+                choice = "Sad";
+            return choice;
+        }
+        public string ValidateEMail(string email)
+        {
+            if (Regex.IsMatch(email, validEmail))
+                choice = "Happy";
+            else
+                choice = "Sad";
+            return choice;
+        }
+        public string ValidateFirstName(string firstName)
+        {
+            if (Regex.IsMatch(firstName, validFirstName))
+                choice = "Happy";
+            else
+                choice = "Sad";
+            return choice;
+        }
+        public string ValidateLastName(string lastName)
+        {
+            if (Regex.IsMatch(lastName, validLastName))
+                choice = "Happy";
+            else
+                choice = "Sad";
+            return choice;
+        }
+        public static void Main()
+        { 
+           
+        }
+    }
+}
