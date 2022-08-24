dc232b7f3b5ead073ada72a86620f3a6452c6374
Jp <jyotiprakashpc@outlook.com>
2022-08-24 21:12:15 +0530

    Validate Mobile Number

---
+++ ProgramUC4.cs
@@ -0,0 +1,18 @@
+﻿namespace RegexProblem
+{
+    public class ProgramUC4
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter Phone Number = ");
+            string phoneNumer = Console.ReadLine();
+
+            ValidatePhoneNumberUC4 validatePhoneNumber = new ValidatePhoneNumberUC4();
+            bool result = validatePhoneNumber.Validate(phoneNumer);
+            if (result)
+                Console.WriteLine("Valid Phone Number");
+            else
+                Console.WriteLine("Invalid Phone Number");
+        }
+    }
+}
---
+++ ValidatePhoneNumberUC4.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegexProblem
+{
+    public class ValidatePhoneNumberUC4
+    {
+        public string validPhoneNumber = "^[9,1]{2}[ ][0-9]{10}$";
+        public bool Validate(string phoneNumber)
+        {
+            return Regex.IsMatch(phoneNumber,validPhoneNumber);
+        }
+    }
+}
