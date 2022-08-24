935b49fc2d08b465f2dc49c8f14584082ec2b14f
Jp <jyotiprakashpc@outlook.com>
2022-08-24 21:15:09 +0530

    Password must contain Uppercase

---
+++ PasswordUpperCaseUC6.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegexProblem
+{
+    public class PasswordUpperCaseUC6
+    {
+        public static string validPassword = "^[a-zA-Z0-9]{8,}$";
+        public bool Validate(string password)
+        {
+            return Regex.IsMatch(password, validPassword);
+        }
+    }
+}
---
+++ ProgramUC6.cs
@@ -0,0 +1,18 @@
+﻿namespace RegexProblem
+{
+    public class ProgramUC6
+    {
+        public static void Main()
+        {
+            Console.Write("Enter A Password Must Contain 8 Character\n1 Upper Case" + " = ");
+            string password = Console.ReadLine();
+
+            PasswordUpperCaseUC6 passwordUpperCaseUC6 = new PasswordUpperCaseUC6();
+            bool result = passwordUpperCaseUC6.Validate(password);
+            if (result)
+                Console.WriteLine("Valid Password");
+            else
+                Console.WriteLine("Invalid Password");
+        }
+    }
+}
