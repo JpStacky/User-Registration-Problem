410f88c860ec5ddd1d2d6cec505182786722528c
Jp <jyotiprakashpc@outlook.com>
2022-08-24 21:16:10 +0530

    Password must contain number

---
+++ PasswordNumericUC7.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegexProblem
+{
+    public class PasswordNumericUC7
+    {
+        public static string validPassword = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z]).{8,}$";
+        public bool Validate(string password)
+        {
+            return Regex.IsMatch(password, validPassword);
+        }
+    }
+}
---
+++ ProgramUC7.cs
@@ -0,0 +1,18 @@
+﻿namespace RegexProblem
+{
+    public class ProgramUC7
+    {
+        public static void Main()
+        {
+            Console.Write("Enter A Password Must Contain 8 Character\n1 Upper Case \n2 Numeric" + " = ");
+            string password = Console.ReadLine();
+
+            PasswordNumericUC7 passwordNumericUC7 = new PasswordNumericUC7();
+            bool result = passwordNumericUC7.Validate(password);
+            if (result)
+                Console.WriteLine("Valid Password");
+            else
+                Console.WriteLine("Invalid Password");
+        }
+    }
+}
