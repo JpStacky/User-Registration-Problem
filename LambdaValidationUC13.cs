af3c32b61c5bae579c78438e82001784cddc6cd0
Jp <jyotiprakashpc@outlook.com>
2022-08-24 21:25:05 +0530

    Use lambda for user data validation

---
+++ LambdaValidationUC13.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexProblem
+{
+    public class LambdaValidationUC13
+    {
+            public string validFirstName = "^[A-Z]{1}[a-z]*$";
+            public string validLastName = "^[A-Z]{1}[a-z]*$";
+            public string validEmail = "^[a-zA-Z0-9]([.]{0,1}[a-zA-Z0-9])*[@]{1}[a-zA-Z0-9]{1,}[.]{1}[a-zA-Z]{2,3}([.]{1}[a-zA-Z]{2,3}){0,1}$";
+            public string validMobileNumber = "^91[ ][0-9]{10}$";
+            public string validPassword = "^(?=.*?[A - Z])(?=.*?[a - z])(?=.*?[0 - 9])(?=.*?[#?!@$%^&*-]).{8,}$";
+
+            public string lambda_Expression_To_Check_Password(string password)
+            => Regex.IsMatch(password, validPassword) ? "Password is Valid" : "Password is Not Valid";
+            
+            public string lambda_Expression_To_Check_Phone_Number(string mobileNumber) 
+            => Regex.IsMatch(mobileNumber, validMobileNumber) ? "phno is Valid" : "phno is not Valid";
+
+            public string lambda_Expression_To_Validate_Email(string email)
+            => Regex.IsMatch(email, validEmail)?"Email Is Valid":"Email Is Not Valid";
+
+            public string lambda_Expression_To_Check_First_Name(string firstName)
+            => Regex.IsMatch(firstName, validFirstName) ? "First Name is Valid" : "First Name is Not Valid";
+
+            public string lambda_Expression_To_Check_Last_Name(string lastName)
+            => Regex.IsMatch(lastName, validLastName) ? "Last Name Is Valid" : "Last Name is Not Valid";  
+    }
+}
+
---
+++ ProgramUC13.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexProblem
+{
+    public class ProgramUC13
+    {
+        public static void Main()
+        {
+            LambdaValidationUC13 lambdaValidationUC13 = new LambdaValidationUC13();
+            string result;
+
+            Console.Write("Enter First Name = ");
+            string firt_Name = Console.ReadLine();
+            result = lambdaValidationUC13.lambda_Expression_To_Check_First_Name(firt_Name);
+            Console.WriteLine(result);
+
+            Console.Write("Enter Last Name = ");
+            string last_Name = Console.ReadLine();
+            result =  lambdaValidationUC13.lambda_Expression_To_Check_Last_Name(last_Name);
+            Console.WriteLine(result);
+
+            Console.Write("Enter Email = ");
+            string email = Console.ReadLine();
+            result = lambdaValidationUC13.lambda_Expression_To_Validate_Email(email);
+            Console.WriteLine(result);
+
+            Console.Write("Enter Mobile Number = ");
+            string mobile_Number = Console.ReadLine();
+            result = lambdaValidationUC13.lambda_Expression_To_Check_Phone_Number(mobile_Number);
+            Console.WriteLine(result);
+
+            Console.Write("Enter Password = ");
+            string password = Console.ReadLine();
+            result = lambdaValidationUC13.lambda_Expression_To_Check_Password(password);
+            Console.WriteLine(result);
+        }
+    }
+}
