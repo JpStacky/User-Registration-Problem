df65810ffea7dc737efc8cc0f3c384d17e78dcc4
Jp <jyotiprakashpc@outlook.com>
2022-08-24 21:17:40 +0530

    Password must contain a special character

---
+++ ProgramUC8.cs
@@ -0,0 +1,18 @@
+﻿namespace RegexProblem
+{
+    public class ProgramUC8
+    {
+        public static void Main()
+        {
+            Console.Write("Enter A Password Must Contain 8 Character\n1 Upper Case \n2 Numeric \n3 Special CHaracter(!@#$%&*) " + " = ");
+            string password = Console.ReadLine();
+
+            PasswordSpecialCharacterUC8 passwordSpecialCharacterUC8 = new PasswordSpecialCharacterUC8();
+            bool result = passwordSpecialCharacterUC8.Validate(password);
+            if (result)
+                Console.WriteLine("Valid Password");
+            else
+                Console.WriteLine("Invalid Password");
+        }
+    }
+}
