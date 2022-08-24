b4bce301cbc11f3d056288266da3a549b554d1ba
Jp <jyotiprakashpc@outlook.com>
2022-08-24 21:08:59 +0530

    Validate First Name

---
+++ ProgramUC1.cs
@@ -0,0 +1,18 @@
+﻿namespace RegexProblem
+{
+    public class ProgramUC1
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter First Name = ");
+            string firstName = Console.ReadLine();
+
+            ValidateFirstNameUC1 validateName = new ValidateFirstNameUC1();
+            bool result = validateName.Validate(firstName);
+            if (result)
+                Console.WriteLine("Valid First Name");
+            else
+                Console.WriteLine("Invalid First Name");
+        }
+    }
+}
---
+++ ValidateFirstNameUC1.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegexProblem
+{
+    public class ValidateFirstNameUC1
+    {
+        public static string validName = "^[A-Z]{1}[a-z]*$";
+        public bool Validate(string firstName)
+        { 
+            return Regex.IsMatch(firstName,validName);
+        }
+    }
+}
