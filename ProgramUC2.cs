1e63d54f3e53aa5c495084d5fdcf0b4af34be8ab
Jp <jyotiprakashpc@outlook.com>
2022-08-24 21:10:08 +0530

    Validate Last Name

---
+++ ProgramUC2.cs
@@ -0,0 +1,18 @@
+﻿namespace RegexProblem
+{
+    public class ProgramUC2
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter Last Name = ");
+            string lastName = Console.ReadLine();
+
+            ValidateLastNameUC2 validateName = new ValidateLastNameUC2();
+            bool result = validateName.Validate(lastName);
+            if (result)
+                Console.WriteLine("Valid Last Name");
+            else
+                Console.WriteLine("Invalid Last Name");
+        }
+    }
+}
---
+++ ValidateLastNameUC2.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegexProblem
+{
+    public class ValidateLastNameUC2
+    {
+        public static string validName = "^[A-Z]{1}[a-z]{2}$";
+        public bool Validate(string lastName)
+        {
+            return Regex.IsMatch(lastName, validName);
+        }
+    }
+}
