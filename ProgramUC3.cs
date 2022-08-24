f46f31881262f42d9da8bfa2303ded922096364d
Jp <jyotiprakashpc@outlook.com>
2022-08-24 21:11:08 +0530

    Validate EmailID

---
+++ ProgramUC3.cs
@@ -0,0 +1,18 @@
+﻿namespace RegexProblem
+{
+    public class ProgramUC3
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter Email = ");
+            string email = Console.ReadLine();
+
+            ValidateEmailUC3 validateEmail = new ValidateEmailUC3();
+            bool result = validateEmail.Validate(email);
+            if (result)
+                Console.WriteLine("Valid Email");
+            else
+                Console.WriteLine("Invalid Email");
+        }
+    }
+}
---
+++ ValidateEmailUC3.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegexProblem
+{
+    public class ValidateEmailUC3
+    {
+        public static string validEmail = "^[a-zA-Z0-9]([.]{0,1}[a-zA-Z0-9])*[@]{1}[a-zA-Z0-9]{1,}[.]{1}[a-zA-Z]{2,3}([.]{1}[a-zA-Z]{2,3}){0,1}$";
+        public bool Validate(string email)
+        {
+            return Regex.IsMatch(email, validEmail);
+        }
+    }
+}
