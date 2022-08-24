c79f6f752397ca594ba26cfb75c5457dbb609733
Jp <jyotiprakashpc@outlook.com>
2022-08-24 21:19:26 +0530

    Email Validation

---
+++ EmailUC9.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegexProblem
+{
+    public class EmailUC9
+    {
+        public static string validEmail = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z])(?=.*[+@#$%^&*!.()-]).{8,}";
+        public bool Validate(string email)
+        {
+            return Regex.IsMatch(email, validEmail);
+        }
+    }
+}
---
+++ ProgramUC9.cs
@@ -0,0 +1,18 @@
+﻿namespace RegexProblem
+{
+    public class ProgramUC9
+    {
+        public static void Main()
+        {
+            Console.Write("Enter A Password Must Contain 8 Character\n1 Upper Case \n2 Numeric \n3 Special CHaracter(!@#$%&*) " + " = ");
+            string email = Console.ReadLine();
+
+            EmailUC9 emailUC9 = new EmailUC9();
+            bool result = emailUC9.Validate(email);
+            if (result)
+                Console.WriteLine("Valid Password");
+            else
+                Console.WriteLine("Invalid Password");
+        }
+    }
+}
