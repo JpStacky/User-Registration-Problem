46c1cc627b3d7e15a59db18fb363eeccc2accfe4
Jp <jyotiprakashpc@outlook.com>
2022-08-24 21:13:45 +0530

    Validate Password

---
+++ Char8PasswordUC5.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegexProblem
+{
+    public class Char8PasswordUC5
+    {
+        public static string validPassword = "^(?=.*?[A - Z])(?=.*?[a - z])(?=.*?[0 - 9])(?=.*?[#?!@$%^&*-]).{8,}$";
+        public bool Validate(string password)
+        {
+            return Regex.IsMatch(password, validPassword);
+        }
+    }
+}
---
+++ ProgramUC5.cs
@@ -0,0 +1,18 @@
+﻿namespace RegexProblem
+{
+    public class ProgramUC5
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter A Password Must Contain 8 Character = ");
+            string password = Console.ReadLine();
+
+            Char8PasswordUC5 char8PasswordUC5 = new Char8PasswordUC5();
+            bool result = char8PasswordUC5.Validate(password);
+            if (result)
+                Console.WriteLine("Valid Password");
+            else
+                Console.WriteLine("Invalid Password");
+        }
+    }
+}
